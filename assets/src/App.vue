c5dc2f0110eac5b4b36ad1ec98f16073cfc82468
seokmin <seokmin@chain.partners>
2018-08-09 15:23:38 +0900

    Integrate vuejs with phoenix

---
+++ assets/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="main">
+    <h1>{{ msg }}</h1>
+    <h2>정빈이가 얼마나 밥을 먹었는지 한번 확인해볼까?</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'main',
+  data () {
+    return {
+      msg: '정빈이 수유 기록표',
+      user: ""
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
