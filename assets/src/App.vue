c364b25a42af3b6fda2ed6bc0284667f30f59f9b
seokmin <seokmin@chain.partners>
2018-08-27 00:32:35 +0900

    Add design on main page with mobile friendly

--- assets/src/App.vue
+++ assets/src/App.vue
@@ -1,36 +1,74 @@
 <template>
-  <div id="landing">
-    <h2>정빈이가 얼마나 밥을 먹었는지 한번 확인해볼까?</h2>
-    <div>
+  <div id="landing" style="background:#ECECEC; padding:30px; height: 100%;">
+    <a-card title="정빈이가 밥을 얼마나 먹었는지 확인해볼까?" :bordered="false" style="width: 100%; height: 100%;">
       <p>정빈이가 먹은 양과 시간을 입력해주세요</p>
-      <input v-model="meal_type" placeholder="분유/모유">
-      <input v-model="volume" placeholder=120>
-      <input v-model="child_name" placeholder="정빈">
-      <input v-model="ate_at" placeholder="201808091637">
-      <button v-on:click="record_meal">저장</button>
-    </div>
-    <div>
-      <ul>
-        <li v-for="history in histories">
-          <p>
-            {{history.child_name}}, {{history.meal_type}}, {{history.volume}}, {{history.ate_at}}
-          </p>
-        </li>
-      </ul>
-    </div>
+      <a-row class="record-list">
+        <a-col :span="12">
+          <a-radio-group :value="milk_type" @change="handleTypeChange">
+            <a-radio-button value="powdered">분유</a-radio-button>
+            <a-radio-button value="breast">모유</a-radio-button>
+          </a-radio-group>
+        </a-col>
+      </a-row>
+      <a-row class="record-list">
+        <a-col :span="10">
+          <a-time-picker :value="milk_time" format="HH:mm" :minuteStep="15" />
+        </a-col>
+        <a-col :span="12" :offset="2">
+          <a-radio-group :value="milk_day" @change="handleDayChange" style="float:right;">
+            <a-radio-button value="today">오늘</a-radio-button>
+            <a-radio-button value="yesterday">어제</a-radio-button>
+          </a-radio-group>
+        </a-col>
+      </a-row>
+      
+      <a-row class="record-list">
+        <a-col :span="16">
+          <a-slider :min="50" :max="240" :step="10" v-model="milk_volume" @afterChange="handleVolumeChange" />
+        </a-col>
+        <a-col :span="8">
+          <a-input-number
+            :min="50"
+            :max="240"
+            :step="10"
+            style="float: right;"
+            v-model="milk_volume"
+            :formatter="value => `${value} ml`"
+          />
+        </a-col>
+      </a-row>
+      <a-button type="primary" v-on:click="record_milk">저장</a-button>
+      <a-divider> Histories </a-divider>
+      <a-list
+        itemLayout="horizontal"
+        :dataSource="histories"
+      >
+        <a-list-item slot="renderItem" slot-scope="item, index">
+          <a-list-item-meta
+            :description="item.ate_at"
+          >
+            <a slot="title">{{item.meal_type == "breast" ? "모유" : "분유"}}</a>
+          </a-list-item-meta>
+          {{item.volume}} ml
+        </a-list-item>
+      </a-list>
+    </a-card>
   </div>
 </template>
 
 <script>
+import moment from 'moment';
+
 export default {
   name: "landing",
   data() {
     return {
-      meal_type: "분유",
-      volume: 0,
+      histories: [],
       child_name: "이정빈",
-      ate_at: "",
-      histories: []
+      milk_type: "breast",
+      milk_volume: 120,
+      milk_day: "today",
+      milk_time: moment(),
     };
   },
   async created() {
@@ -38,17 +76,29 @@ export default {
     this.histories = action_records.actions.map(
       action => action.action_trace.act.data
     );
-    console.log(this.histories);
   },
   methods: {
-    record_meal: async function() {
+    moment,
+    handleDayChange(e) {
+      this.milk_day = e.target.value;
+    },
+    handleTypeChange(e) {
+      this.milk_type = e.target.value;
+    },
+    handleVolumeChange(value) {
+      this.milk_volume = value;
+    },
+    record_milk: async function() {
+      let milk_type = this.milk_type == "breast" ? "모유":"분유";
+      let report_date = this.milk_day == "today" ? moment().format("YYYYMMDD") : moment().subtract(1, 'days').format("YYYYMMDD");
+      let milk_ate_time = report_date + " " + this.milk_time.format("h:mma");
       let tx_write = await this.$parent.eos.transaction("daybit", daybit => {
         daybit.write(
           "daybit",
-          this.meal_type,
+          milk_type,
           this.child_name,
-          Number(this.volume),
-          this.ate_at,
+          this.milk_volume,
+          milk_ate_time,
           this.$parent.options
         );
       });
@@ -58,27 +108,7 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-h1,
-h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.record-list {
+  margin-bottom: 10px;
 }
 </style>
--- assets/src/app.js
+++ assets/src/app.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
 import Eosjs from 'eosjs';
 import App from './App.vue';
-// import Main from './Main.vue'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+
+Vue.use(Antd);
 
 new Vue({
   el: '#app',
@@ -9,7 +12,7 @@ new Vue({
     const config = {
       chainId: "038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca",
       keyProvider: ["5KRfhNCjRqixYZsdvoLyM2ZgeUm8SrjoKN56HFCxw2a9AsQnaP7"],
-      httpEndpoint: "http://alex.chain.partners:8832",
+      httpEndpoint: "http://seokmin.chain.partners:8888",
       expireInSeconds: 60,
       broadcast: true,
       verbose: false,
